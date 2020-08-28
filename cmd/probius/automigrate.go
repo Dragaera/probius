e86d70bb43db03d2a84d668e275f5439097603a8
Michael Senn <michael@morrolan.ch>
2020-08-28 19:18:24 +0200

    Add automigrate command.
    Allows running migrations selectively, instead of implicitly on start.

---
+++ cmd/probius/automigrate.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/dragaera/probius/internal/config"
+	"github.com/dragaera/probius/internal/persistence"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func main() {
+	err := godotenv.Load(".env.development")
+	if err != nil {
+		log.Print("Error loading .env file: ", err)
+	}
+
+	// Will `log.Fatal()` if an env variable is missing
+	cfg := config.ConfigFromEnv()
+
+	orm, err := persistence.InitializeORM(cfg.DB)
+	if err != nil {
+		log.Fatal("Error while initializing ORM persistence layer: ", err)
+	}
+
+	log.Print("Starting automigration.")
+	orm.AutoMigrate(
+		&persistence.DiscordUser{},
+		&persistence.DiscordGuild{},
+		&persistence.DiscordChannel{},
+
+		&persistence.SC2ReplayStatsUser{},
+		&persistence.Tracking{},
+	)
+}
